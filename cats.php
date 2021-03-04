229b712e733d6fabd783b74301cceb69a7fb5437
Albin Lagerquist <lagerquist.albin@gmail.com>
2021-03-04 14:34:34 +0100

    Tidy up code

--- cats.php
+++ cats.php
@@ -20,55 +20,68 @@
 
 		 <div class="cat-box">
 			 <h1>Cat facts bitch</h1>
-			 <form class="cat-form" method="post">
+			 <form class="cat-form" method="get">
 			 		<button type="submit" name="fact">Get fact</button>
           <button type="submit" name="breed">Get breed</button>
-    
+
           <?php
-          
-            if (isset($_POST['fact'])) {
-              
+
+            if (isset($_GET['fact'])) {
+
+							// initiate curl & set url
               $curlconn = curl_init();
               $url = "https://catfact.ninja/fact?max_length=50";
-            
+
+							// set curl options, the url and to return as string
               curl_setopt($curlconn, CURLOPT_URL, $url);
               curl_setopt($curlconn, CURLOPT_RETURNTRANSFER, true);
-    
+
+							// excecute curl and save in variable
               $response = curl_exec($curlconn);
-    
+
+							// decode json file retrieved
               $jsonResponse = json_decode($response, true);
 
-              echo "<h3>".$jsonResponse['fact']."</h3>";
+							// save the fact as variable
+							$fact = $jsonResponse['fact'];
+
+							// print the fact
+              echo "<h3>".$fact."</h3>";
 
               curl_close($curlconn);
             }
 
-            if (isset($_POST['breed'])) {
-              
+            if (isset($_GET['breed'])) {
+
+							// initiate curl & set url
               $curlconn = curl_init();
               $url = "https://catfact.ninja/breeds?limit=98";
-            
+
+							// set curl options, the url and to return as string
               curl_setopt($curlconn, CURLOPT_URL, $url);
               curl_setopt($curlconn, CURLOPT_RETURNTRANSFER, true);
-    
-              $response = curl_exec($curlconn);
-              $jsonResponse = json_decode($response, true);
-              $randomBreed = rand(1, 98);
-             
-              echo "<h3>".$jsonResponse['data'][$randomBreed]['breed']."</h3>";
 
+							// excecute curl and save in variable
+              $response = curl_exec($curlconn);
+
+							// decode json file retrieved
+              $jsonResponse = json_decode($response, true);
+
+							// set random number between 0-97 to get a random breed from array
+              $randomNumber = rand(0, 97);
+
+							// save breed as variable, randomNumber gives a random item from array
+							$breed = $jsonResponse['data'][$randomNumber]['breed'];
+
+							// print the breed
+              echo "<h3>".$breed."</h3>";
+
+							// close curl connection
               curl_close($curlconn);
-        
-            //  $output = "<ul>";
-            //  foreach($jsonResponse['data'] as $each) {
-            //    $output .= "<li>".$each['breed']."</li>";
-            //  }
-            //  $output .= "</ul>";
-            //  echo $output;
+
             }
 
           ?>
-          </ul>
 			 </form>
 		 </div>
 
--- gallery.php
+++ gallery.php
@@ -19,37 +19,37 @@
 		 <div class="gallery-box">
 
 				<h1>Gallery</h1>
-				<form method="post">
+				<form method="get">
 					<button name="add-button">New Image</button>
 				</form>
 
         <div class="gallery-item-container">
 					<?php
-						
-						if (isset($_POST['add-button'])) {
-							
+
+						if (isset($_GET['add-button'])) {
+
 							// Initiate curl
 							$curlconn = curl_init();
 
-							// get random array item from list retrievef from the url
-							$randomImage = rand(0, 99);
+							// save url in variable
 							$url = "https://picsum.photos/v2/list?page=2&limit=100";
-						
+
 							// specify options for curl
 							curl_setopt($curlconn, CURLOPT_URL, $url);
 							curl_setopt($curlconn, CURLOPT_RETURNTRANSFER, true);
-							
+
 							// excecute curl and save in variable
 							$picture = curl_exec($curlconn);
 
 							// decode json file retrieved
 							$jsonResponse = json_decode($picture, true);
-							
-							
 
-							// save url and author key in variables
-							$print = $jsonResponse[$randomImage]['download_url'];
-							$author = $jsonResponse[$randomImage]['author'];
+							// set variable to get random array item from list retrieved from the url
+							$randomNumber = rand(0, 99);
+
+							// save url and author key in variables (to make it cleaner when echo)
+							$print = $jsonResponse[$randomNumber]['download_url'];
+							$author = $jsonResponse[$randomNumber]['author'];
 
 							// print out the actual image
 							echo "<img class='gallery-item-new' src=".$print.">";
@@ -62,7 +62,7 @@
 
 
 							// echo "<img class ='gallery-item-new' src='https://picsum.photos/id/".$randomId."/2500/1667'><br>";
-							
+
 							// foreach($jsonResponse as $each) {
 							// 	echo $each['author'];
 							// }
@@ -81,17 +81,15 @@
 							 // }
 							 // $output2 .= "</ul>";
 							 // echo $output2;
-          				
+
 							// echo '<img src="'.$url.'">';
 						}
 
-						
-
 						// return an array of filenames
-					//	$images = glob("uploads/*.*");
-					//	foreach($images as $image) {
-					//		echo '<img class="gallery-item" src="'.$image.'" />';
-					//	}
+						$images = glob("uploads/*.*");
+						foreach($images as $image) {
+							echo '<img class="gallery-item" src="'.$image.'" />';
+						}
 					?>
 				</div>
 
