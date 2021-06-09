8f17c61da19ac16ba0823847625e6ace5959a09b
Albin Lagerquist <lagerquist.albin@gmail.com>
2021-06-10 01:58:25 +0200

    stuff

--- login-page.php
+++ login-page.php
@@ -30,7 +30,6 @@
               <p class="error"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather-alert-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg><?php echo $_GET['error'] ?></p>
             <?php } ?>
 			 		<button type="submit" value="Login">Login</button>
-
 			 </form>
 		 </div>
 
